e9882d2893684dbe134f2fdbc5e87d0d4cf424f9
YdrMaster <ydrml@hotmail.com>
2023-07-14 15:51:19 +0800

    build: add fmt
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 框架 X
+
+## 依赖
+
+- [fmt v10.0.0](https://github.com/fmtlib/fmt/releases/tag/10.0.0)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-﻿#include <iostream>
+﻿#include <fmt/core.h>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    fmt::print("Hello, world!\n");
     return 0;
 }
