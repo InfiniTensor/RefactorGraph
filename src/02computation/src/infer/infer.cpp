bbedb7ac97837f93569c9237b779db037b9e19cf
YdrMaster <ydrml@hotmail.com>
2023-09-08 09:24:32 +0800

    feat: 推导 CumSum Slice
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/02computation/src/infer/infer.cpp
+++ src/02computation/src/infer/infer.cpp
@@ -306,16 +306,107 @@ namespace refactor::graph {
     InferResult inferCumSum(NodeInfo const &node, Edges inputs) {
         if (inputs.size() != 2) {
             return Err(InferError(ERROR_MSG("Input size error")));
+        } else if (!inputs[1]->shape.empty()) {
+            return Err(InferError(ERROR_MSG("Input shape not support")));
+        } else if (!isNumbericDataType(inputs[0]->dataType) ||
+                   (inputs[1]->dataType != DataType::I64 &&
+                    inputs[1]->dataType != DataType::I32)) {
+            return Err(InferError(ERROR_MSG("Input data type not support")));
         } else {
-            return Err(InferError(ERROR_MSG("Not implemented")));
+            return Ok(Edges{std::move(inputs[0])});
         }
     }
 
     InferResult inferSlice(NodeInfo const &node, Edges inputs) {
-        if (inputs.size() != 2) {
+        if (inputs.size() < 3 || 5 < inputs.size()) {
             return Err(InferError(ERROR_MSG("Input size error")));
         } else {
-            return Err(InferError(ERROR_MSG("Not implemented")));
+            auto const &data = inputs[0];
+            auto const &starts_ = inputs[1];
+            auto const &ends_ = inputs[2];
+            auto tint = starts_->dataType;
+            if (tint != DataType::I32 && tint != DataType::I64) {
+                return Err(InferError(ERROR_MSG("Data type not support")));
+            }
+            if (ends_->dataType != tint) {
+                return Err(InferError(ERROR_MSG("Data type not support")));
+            }
+            if (starts_->shape.size() != 1) {
+                return Err(InferError(ERROR_MSG("Input shape not support")));
+            }
+            auto rank = starts_->shape[0].value();
+            if (ends_->shape.size() != 1 || ends_->shape[0].value() != rank) {
+                return Err(InferError(ERROR_MSG("Input shape not support")));
+            }
+            if (data->shape.size() != rank) {
+                return Err(InferError(ERROR_MSG("Input shape not support")));
+            }
+            if (!starts_->hasData() || !ends_->hasData()) {
+                return Err(InferError(ERROR_MSG("Starts and ends must be constant")));
+            }
+            int64_t *starts = reinterpret_cast<int64_t *>(starts_->data->ptr),
+                    *ends = reinterpret_cast<int64_t *>(ends_->data->ptr),
+                    *axes = nullptr,
+                    *steps = nullptr;
+            std::vector<int64_t> axes__, steps__;
+            if (inputs.size() < 4) {
+                axes__.resize(rank);
+                axes = axes__.data();
+                for (int64_t i = 0; i < rank; ++i) {
+                    axes[i] = i;
+                }
+            } else {
+                auto const &axes_ = inputs[3];
+                if (axes_->dataType != tint || axes_->shape != starts_->shape) {
+                    return Err(InferError(ERROR_MSG("Axes not support")));
+                }
+                if (!axes_->hasData()) {
+                    return Err(InferError(ERROR_MSG("Axes must be constant")));
+                }
+                axes = reinterpret_cast<int64_t *>(axes_->data->ptr);
+            }
+            if (inputs.size() < 5) {
+                steps__.resize(rank, 1);
+                steps = steps__.data();
+            } else {
+                auto const &steps_ = inputs[4];
+                if (steps_->dataType != tint || steps_->shape != starts_->shape) {
+                    return Err(InferError(ERROR_MSG("Steps not support")));
+                }
+                if (!steps_->hasData()) {
+                    return Err(InferError(ERROR_MSG("Steps must be constant")));
+                }
+                steps = reinterpret_cast<int64_t *>(steps_->data->ptr);
+            }
+
+            Shape ans(rank, DimExpr(1));
+            for (size_t i = 0; i < rank; ++i) {
+                auto axis = axes[i];
+                auto start = starts[i];
+                auto end = ends[i];
+                auto step = steps[i];
+                if (axis < 0) {
+                    axis += rank;
+                }
+                auto dim = data->shape[axis].value();
+                if (start < 0) {
+                    start += dim;
+                }
+                if (end < 0) {
+                    end += dim;
+                }
+                if (start < 0 || dim <= start || end < 0 || dim < end) {
+                    return Err(InferError(ERROR_MSG("Input shape not support")));
+                }
+                if (step > 0) {
+                    ans[axis] = DimExpr((end - start + step - 1) / step);
+                } else if (step < 0) {
+                    ans[axis] = DimExpr((end - start - step + 1) / -step);
+                } else {
+                    return Err(InferError(ERROR_MSG("Input shape not support")));
+                }
+            }
+            return Ok(Edges{std::make_shared<Tensor>(data->dataType, std::move(ans))});
         }
     }
 
@@ -441,10 +532,12 @@ namespace refactor::graph {
     }
 
     InferResult inferSoftmax(NodeInfo const &node, Edges inputs) {
-        if (inputs.size() != 2) {
+        if (inputs.size() != 1) {
             return Err(InferError(ERROR_MSG("Input size error")));
+        } else if (!isIeee754DataType(inputs[0]->dataType)) {
+            return Err(InferError(ERROR_MSG("Input data type not support")));
         } else {
-            return Err(InferError(ERROR_MSG("Not implemented")));
+            return Ok(std::move(inputs));
         }
     }
 
@@ -467,8 +560,10 @@ namespace refactor::graph {
     }
 
     InferResult inferReduce(NodeInfo const &node, Edges inputs) {
-        if (inputs.size() != 2) {
+        if (inputs.empty() || 2 < inputs.size()) {
             return Err(InferError(ERROR_MSG("Input size error")));
+        } else if (!isNumbericDataType(inputs[0]->dataType)) {
+            return Err(InferError(ERROR_MSG("Input data type not support")));
         } else {
             return Err(InferError(ERROR_MSG("Not implemented")));
         }
