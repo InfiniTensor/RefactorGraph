0ec157b1dd35a91095d29868c539c9bd1fce14a5
YdrMaster <ydrml@hotmail.com>
2023-09-13 12:11:31 +0800

    style: 整理 Gather
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/03onnx/src/infer/gather.cpp
+++ src/03onnx/src/infer/gather.cpp
@@ -8,8 +8,10 @@ namespace refactor::onnx {
         if (inputs[1]->dataType != DataType::I32 && inputs[1]->dataType != DataType::I64) {
             return Err(InferError(ERROR_MSG("Input data type not support")));
         } else {
-            auto const r = inputs[0]->shape.size();
-            auto const q = inputs[1]->shape.size();
+            auto const &data = inputs[0];
+            auto const &indices = inputs[1];
+            auto const r = data->shape.size();
+            auto const q = indices->shape.size();
             auto axis = op.attribute("axis", {0}).int_();
             if (axis < 0) {
                 axis += r;
@@ -17,58 +19,52 @@ namespace refactor::onnx {
             if (axis < 0 || r <= axis) {
                 return Err(InferError(ERROR_MSG("Input shape not support")));
             }
-            auto dataType = inputs[0]->dataType;
-            auto ans = inputs[0]->shape;
-            auto const &indices = inputs[1]->shape;
-            ans.erase(ans.begin() + axis);
-            ans.insert(ans.begin() + axis, indices.begin(), indices.end());
-            if (!shouldCalculate(inputs, ans)) {
-                return Ok(Edges{std::make_shared<Tensor>(dataType, std::move(ans))});
+            auto dataType = data->dataType;
+            auto output = data->shape;
+            output.erase(output.begin() + axis);
+            output.insert(output.begin() + axis, indices->shape.begin(), indices->shape.end());
+            if (!shouldCalculate(inputs, output)) {
+                return Ok(Edges{std::make_shared<Tensor>(dataType, std::move(output))});
             }
 
-            auto const ssz = ans.size();
-            absl::InlinedVector<int64_t, 4> sX(r), sY(q);
-
-            auto getter = [](DimExpr const &d) { return d.value(); };
-            std::transform(inputs[0]->shape.begin(), inputs[0]->shape.end(), sX.begin(), getter);
-            std::transform(inputs[1]->shape.begin(), inputs[1]->shape.end(), sY.begin(), getter);
-
-            auto [sZ, size] = shape_size(ans);
-            auto eleSize = dataTypeSize(inputs[0]->dataType);
+            auto const ssz = output.size();
+            auto [sZ, size] = shape_size(output);
+            auto eleSize = dataTypeSize(dataType);
             auto blob = std::make_shared<Blob>(new uint8_t[size * eleSize]);
-            auto srcX = reinterpret_cast<uint8_t *>(inputs[0]->data->ptr);
-            auto srcY = reinterpret_cast<int64_t *>(inputs[1]->data->ptr);
+            auto src = reinterpret_cast<uint8_t *>(data->data->ptr);
             auto dst = reinterpret_cast<uint8_t *>(blob->ptr);
 
             for (size_t i = 0; i < size; ++i) {
-                auto indices = buildIndices(sZ, i);
-
-                size_t ii = 0, mul = 1;
-                for (size_t j = 0; j < q; ++j) {
-                    auto j_ = q - 1 - j;// reverse
-                    ii += indices[j_] * mul;
-                    mul *= sY[j_];
+                auto indices_ = buildIndices(sZ, i);
+                int64_t k;
+                {
+                    size_t ii = 0, mul = 1;
+                    for (size_t j = 0; j < q; ++j) {
+                        auto j_ = q - 1 - j;// reverse
+                        ii += indices_[j_] * mul;
+                        mul *= indices->shape[j_].value();
+                    }
+                    k = reinterpret_cast<int64_t *>(indices->data->ptr)[ii];
                 }
-                auto k = srcY[ii];
-
-                ii = 0, mul = 1;
-                for (size_t j = axis + q; j < ssz; ++j) {
-                    auto j_ = ssz - 1 - j;// reverse
-                    ii += indices[j_] * mul;
-                    mul *= sX[j_ - q + 1];
+                {
+                    size_t ii = 0, mul = 1;
+                    for (size_t j = axis + q; j < ssz; ++j) {
+                        auto j_ = ssz - 1 - j;// reverse
+                        ii += indices_[j_] * mul;
+                        mul *= data->shape[j_ - q + 1].value();
+                    }
+                    ii += k * mul;
+                    for (size_t j = 0; j < axis; ++j) {
+                        auto j_ = axis - 1 - j;// reverse
+                        ii += indices_[j_] * mul;
+                        mul *= data->shape[j_].value();
+                    }
+                    std::copy_n(src + ii * eleSize, eleSize, dst + i * eleSize);
                 }
-                ii += k * mul;
-                for (size_t j = 0; j < axis; ++j) {
-                    auto j_ = axis - 1 - j;// reverse
-                    ii += indices[j_] * mul;
-                    mul *= sX[j_];
-                }
-
-                std::copy_n(srcX + ii * eleSize, eleSize, dst + i * eleSize);
                 fmt::println("gather copies {} bytes", eleSize);
             }
 
-            return Ok(Edges{std::make_shared<Tensor>(dataType, std::move(ans), std::move(blob))});
+            return Ok(Edges{std::make_shared<Tensor>(dataType, std::move(output), std::move(blob))});
         }
     }
 
