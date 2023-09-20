d78c70dbe9872b6b17ca32525cb1210edfeae0fb
YdrMaster <ydrml@hotmail.com>
2023-09-20 12:08:07 +0800

    feat: 支持范围反向迭代
    fix: 改正 gather 并支持并行/向量化计算
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/03onnx/src/infer/gather.cpp
+++ src/03onnx/src/infer/gather.cpp
@@ -1,5 +1,6 @@
 ﻿#include "common/range.h"
 #include "infer.h"
+#include <execution>
 
 namespace refactor::onnx {
     using namespace refactor::common;
@@ -34,16 +35,15 @@ namespace refactor::onnx {
             auto src = reinterpret_cast<uint8_t *>(data->data->ptr);
             auto dst = reinterpret_cast<uint8_t *>(ans->malloc());
 
-            std::for_each_n(natural_t(0), ans->elementsSize(),
+            std::for_each_n(std::execution::par_unseq, natural_t(0), ans->elementsSize(),
                             [&, src, dst, eleSize, ssz, q](auto i) {
                                 auto indices_ = locateN(output, i);
                                 int64_t k;
                                 {
                                     size_t ii = 0, mul = 1;
-                                    for (size_t j = 0; j < q; ++j) {
-                                        auto j_ = q - 1 - j;// reverse
-                                        ii += indices_[j_] * mul;
-                                        mul *= indices->shape[j_].value();
+                                    for (auto j : range0_(q).rev()) {
+                                        ii += indices_[j] * mul;
+                                        mul *= indices->shape[j].value();
                                     }
                                     k = indices->dataType == DataType::I64
                                             ? reinterpret_cast<int64_t *>(indices->data->ptr)[ii]
@@ -51,16 +51,14 @@ namespace refactor::onnx {
                                 }
                                 {
                                     size_t ii = 0, mul = 1;
-                                    for (size_t j = axis + q; j < ssz; ++j) {
-                                        auto j_ = ssz - 1 - j;// reverse
-                                        ii += indices_[j_] * mul;
+                                    for (auto j : range(static_cast<decltype(q)>(axis) + q, ssz).rev()) {
+                                        ii += indices_[j] * mul;
                                         mul *= data->shape[j - q + 1].value();
                                     }
                                     ii += k * mul;
-                                    for (size_t j = 0; j < axis; ++j) {
-                                        auto j_ = axis - 1 - j;// reverse
-                                        ii += indices_[j_] * mul;
-                                        mul *= data->shape[j_].value();
+                                    for (auto j : range0_(axis).rev()) {
+                                        ii += indices_[j] * mul;
+                                        mul *= data->shape[j].value();
                                     }
                                     std::memcpy(dst + i * eleSize, src + ii * eleSize, eleSize);
                                 }
