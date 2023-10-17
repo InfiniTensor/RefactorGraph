70da22385c9c959846a37b220b5eb6073c1178df
YdrMaster <ydrml@hotmail.com>
2023-10-17 09:50:00 +0800

    feat: 分析 einsum 表达式
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/07onnx/src/operators/einsum.cc
+++ src/07onnx/src/operators/einsum.cc
@@ -25,6 +25,71 @@ namespace refactor::onnx {
         if (inputs.empty()) {
             return Err(InferError(ERROR_MSG("Input size error")));
         }
+
+        std::vector<uint_lv2> indices;
+        std::vector<char> data;
+        {
+            auto implicit = true;
+            for (size_t i = 0; i < equation.size();) {
+                switch (equation[i]) {
+                    case ' ':
+                        ++i;
+                        break;
+
+                    case ',':
+                        ++i;
+                        ASSERT(implicit, "");
+                        indices.push_back(data.size());
+                        break;
+
+                    case '-':
+                        ASSERT(i + 1 < equation.size(), "");
+                        ASSERT(equation[i + 1] == '>', "");
+                        i += 2;
+                        implicit = false;
+                        indices.push_back(data.size());
+                        break;
+
+                    case '.':
+                        ASSERT(i + 2 < equation.size(), "");
+                        ASSERT(equation[i + 1] == '.' && equation[i + 2] == '.', "");
+                        i += 3;
+                        data.push_back('.');
+                        break;
+
+                    default:
+                        if ('a' <= equation[i] && equation[i] <= 'z') {
+                            data.push_back(equation[i++]);
+                        } else {
+                            UNREACHABLE();
+                        }
+                        break;
+                }
+            }
+            if (implicit) {
+                indices.push_back(data.size());
+            }
+        }
+        // ---
+        {
+            auto implicit = indices.back() == data.size();
+            std::string msg;
+            if (!implicit) {
+                for (auto i : range(indices.rbegin()[0], static_cast<uint_lv2>(data.size()))) {
+                    msg += data[i];
+                }
+                msg += " = ";
+            }
+            uint_lv2 begin = 0;
+            for (auto i : range0_(indices.size())) {
+                for (auto j : range(begin, indices[i])) {
+                    msg += data[j];
+                }
+                msg += " * ";
+                begin = indices[i];
+            }
+            fmt::println("{}", msg.substr(0, msg.size() - 3));
+        }
         TODO("");
     }
 
