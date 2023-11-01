bb65eaa0a3d7446e43bf9ec2cecd822911217f4e
YdrMaster <ydrml@hotmail.com>
2023-11-01 17:39:15 +0800

    style(mem_manager): 整理偏移计算器
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/02mem_manager/include/mem_manager/functions.h
@@ -0,0 +1,19 @@
+﻿#ifndef MEM_MANAGER_FUNCTIONS_H
+#define MEM_MANAGER_FUNCTIONS_H
+
+#include <cstddef>
+
+namespace refactor::mem_manager {
+
+    constexpr size_t align2power(size_t size, int bits) {
+        auto mask = (1 << bits) - 1;
+        return (size + mask) & ~mask;
+    }
+
+    constexpr size_t alignBytes(size_t size, int bytes) {
+        return (size + bytes - 1) / bytes * bytes;
+    }
+
+}// namespace refactor::mem_manager
+
+#endif// MEM_MANAGER_FUNCTIONS_H
