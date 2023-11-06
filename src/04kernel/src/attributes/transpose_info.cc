0d643e18eac724ac1a11bb964f37d148ea1e596e
YdrMaster <ydrml@hotmail.com>
2023-11-06 18:22:16 +0800

    docs(kernel): 整理 TransposeInfo，添加注释
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/04kernel/src/attributes/transpose_info.cc
+++ src/04kernel/src/attributes/transpose_info.cc
@@ -11,12 +11,18 @@ namespace refactor::kernel {
         // 对 permutation 分段，每个段里是一些连续的维度号
         // 第 i 个 perm 到其所在的第 j 个段的映射
         constexpr static auto INVALID = std::numeric_limits<uint_lv2>::max();
-        std::vector<uint_lv2> dims_(rank, INVALID);
-        uint_lv2 segs = 0, p = INVALID;
-        for (auto i : range0_(rank)) {
-            if (shape[perm[i]] != 1) {
-                auto last = std::exchange(p, perm[i]);
-                dims_[p] = p != last + 1 ? ++segs : segs;
+        std::vector<uint_lv2> segOfDims(rank, 0);
+        uint_lv2 segs = 0, lastPerm = perm[0];
+        for (auto i : range(1ul, rank)) {
+            if (auto p = perm[i]; p == lastPerm + 1) {
+                // 维度连续，不分段
+                segOfDims[lastPerm = p] = segs;
+            } else if (shape[p] != 1) {
+                // 维度不连续，形状不为 1，分段
+                segOfDims[lastPerm = p] = ++segs;
+            } else {
+                // 形状 1 不会导致分段
+                segOfDims[p] = INVALID;
             }
         }
 
@@ -24,16 +30,14 @@ namespace refactor::kernel {
         struct SizePerm {
             uint_lv2 sizeI, perm, sizeO, strideI, strideO;
         };
-        absl::InlinedVector<SizePerm, 4> forward(segs);
+        absl::InlinedVector<SizePerm, 4> forward(++segs);
         uint_lv2 j = 0;
-        p = INVALID;
+        auto seg = INVALID;
         for (auto i : range0_(rank)) {
-            if (dims_[i] == INVALID) {
-                continue;
-            }
-            auto last = std::exchange(p, dims_[i] - 1);
-            if (p != last) {
-                forward[++j - 1] = {shape[i], p, 1, 1, 1};
+            if (segOfDims[i] == INVALID) { continue; }
+            auto last = std::exchange(seg, segOfDims[i]);
+            if (seg != last) {
+                forward[j++] = {shape[i], seg, 1, 1, 1};
             } else {
                 forward[j - 1].sizeI *= shape[i];
             }
