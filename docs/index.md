7c32f119c770fdbcf72d262e3ef3aa6afd6d9731
YdrMaster <ydrml@hotmail.com>
2023-11-09 08:50:15 +0800

    feat: 尝试 oneflow 的 element-wise 优化但无效
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- docs/index.md
+++ docs/index.md
@@ -11,5 +11,10 @@
 ### Nvidia 优化
 
 - [深入浅出 GPU 优化系列](https://www.zhihu.com/people/ban-zhuan-yuan-shi-chao-ji-gun/posts)
+- Element-wise
+  - [OneFlow Element-Wise 算子源码解读](https://zhuanlan.zhihu.com/p/646990764)
+  - [解析OneFlow Element-Wise 算子实现](https://zhuanlan.zhihu.com/p/591058808)
+  - [Cuda 高性能计算经典模式 逐位计算(elementwise)](https://zhuanlan.zhihu.com/p/663951645)
+  - [高效、易用、可拓展我全都要：OneFlow CUDA Elementwise 模板库的设计优化思路](https://zhuanlan.zhihu.com/p/447577193)
 - [Transpose](https://zhuanlan.zhihu.com/p/582664676)
 - [Reduce](https://zhuanlan.zhihu.com/p/559549740)
--- src/04kernel/cuda/src/naive.cu
+++ src/04kernel/cuda/src/naive.cu
@@ -11,7 +11,124 @@ __global__ static void sigmoidKernel(float *out, const float *in, const int N) {
     }
 }
 
+struct SigmoidFunctor {
+    __device__ __host__ __forceinline__ float operator()(float x) const {
+        return 1.0 / (1.0 + expf(-x));
+    }
+};
+
+struct SigmoidFactory {
+    __device__ __host__ __forceinline__ SigmoidFunctor operator()() const {
+        return SigmoidFunctor();
+    }
+};
+
 namespace refactor::kernel::cuda {
+
+    template<class T, int N>
+    struct Packed {
+        using Phantom = typename std::aligned_storage<N * sizeof(T), N * sizeof(T)>::type;
+
+        union {
+            T elem[N];
+            Phantom _phantom;
+        };
+
+        __device__ Packed() = default;
+    };
+
+    template<class T>
+    constexpr size_t nPack() {
+        static_assert(sizeof(T) > 1);
+        constexpr size_t AccessGrainBits = 128;
+        return AccessGrainBits / 8 / sizeof(T);
+    }
+
+    template<class T, class U, class... Args>
+    constexpr size_t nPack() {
+        return std::min(nPack<T>(), nPack<U, Args...>());
+    }
+
+    template<size_t>
+    constexpr bool isAlignedForPack() {
+        return true;
+    }
+
+    template<size_t N, class T, class... Others>
+    constexpr bool isAlignedForPack(T const *ptr, Others const *...others) {
+        return reinterpret_cast<uintptr_t>(ptr) % sizeof(Packed<T, N>) == 0 &&
+               isAlignedForPack<N>(others...);
+    }
+
+    template<int N, class FunctorT> class UseApply2 {
+        struct A {};
+        struct B {};
+
+        template<class C> static A test(decltype(&C::Apply2));
+        template<class C> static B test(...);
+
+    public:
+        constexpr static bool value =
+            N % 2 == 0 &&
+            std::is_same_v<decltype(test<FunctorT>(0)), A>;
+    };
+
+    template<class Ans, class FunctorT, int N, class... Args>
+    __device__ typename std::enable_if_t<UseApply2<N, FunctorT>::value, Packed<Ans, N>>
+    applyPack(FunctorT const &functor, Packed<Args, N> const... args) {
+        Packed<Ans, N> ans;
+#pragma unroll
+        for (int j = 0; j < N; j += 2) { functor.Apply2(ans.elem + j, (args.elem + j)...); }
+        return ans;
+    }
+
+    template<class Ans, class FunctorT, int N, class... Args>
+    __device__ typename std::enable_if_t<!UseApply2<N, FunctorT>::value, Packed<Ans, N>>
+    applyPack(const FunctorT &functor, Packed<Args, N> const... args) {
+        Packed<Ans, N> ans;
+#pragma unroll
+        for (int j = 0; j < N; ++j) { ans.elem[j] = functor((args.elem[j])...); }
+        return ans;
+    }
+
+    template<int N, class Ans, class FactoryT, class... Args>
+    __global__ void applyGeneric(
+        FactoryT factory,
+        int64_t n, int64_t nPack,
+        Ans *ans, Args const *...args) {
+        auto functor = factory();
+        size_t tid = blockIdx.x * blockDim.x + threadIdx.x,
+               tail = tid + nPack * N,
+               step = blockDim.x * gridDim.x;
+        for (; tid < nPack; tid += step) {
+            reinterpret_cast<Packed<Ans, N> *>(ans)[tid] = applyPack<Ans>(functor, (reinterpret_cast<Packed<Args, N> const *>(args)[tid])...);
+        }
+        if (tail < n) {
+            ans[tail] = functor((args[tail])...);
+        }
+    }
+
+    template<class FactoryT, class Ans, class... Args>
+    cudaError_t launch(ThreadsDistributer const &distributer,
+                       FactoryT factory,
+                       int64_t n, Ans *ans, Args const *...args,
+                       cudaStream_t stream) {
+        constexpr auto NPack = nPack<Ans, Args...>();
+        if (isAlignedForPack<NPack>(ans, args...)) {
+            int64_t const nPack = n / NPack;
+            auto [grid, block] = distributer(nPack);
+            applyGeneric<NPack, Ans><<<grid, block, 0, stream>>>(
+                factory, n, nPack,
+                ans, args...);
+        } else {
+            auto [grid, block] = distributer(n);
+            applyGeneric<1, Ans><<<grid, block, 0, stream>>>(
+                factory, n, n,
+                ans, args...);
+        }
+
+        return cudaPeekAtLastError();
+    }
 
     void sigmoid(float *out, float const *in, unsigned long long n) {
         thrust::device_vector<float>
@@ -23,7 +140,7 @@ namespace refactor::kernel::cuda {
 
         auto t0 = std::chrono::high_resolution_clock::now();
         for (auto i = 0; i < 1000; ++i)
-            sigmoidKernel<<<grid, block>>>(out_.data().get(), in_.data().get(), n);
+            launch<SigmoidFactory, float, float>(distributer, SigmoidFactory(), n, out_.data().get(), in_.data().get(), 0);
         cudaDeviceSynchronize();
         auto t1 = std::chrono::high_resolution_clock::now();
         fmt::println("gpu: {} ns", std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count());
