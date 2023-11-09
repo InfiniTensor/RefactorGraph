d847771879a995299053a48b925e6518f098e892
YdrMaster <ydrml@hotmail.com>
2023-11-09 16:23:40 +0800

    build(kernel): 手写 transpose kernel，放弃 thrust
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/04kernel/cuda/src/transpose.cu
@@ -0,0 +1,44 @@
+﻿#include "kernel/cuda/transpose.cuh"
+#include <cstdint>
+
+namespace refactor::kernel::cuda {
+
+    __global__ static void transposeKernel(
+        unsigned long long n,
+        uint8_t const *data, DimStride const *strides, uint8_t *output,
+        unsigned int rank,
+        unsigned int eleSize) {
+        for (auto tid = blockIdx.x * blockDim.x + threadIdx.x,
+                  step = blockDim.x * gridDim.x;
+             tid < n;
+             tid += step) {
+            auto j = 0u, rem = tid;
+            for (auto k = 0u; k < rank; ++k) {
+                auto d = strides[k];
+                j += rem / d.o * d.i;
+                rem %= d.o;
+            }
+
+            memcpy(output + tid * eleSize, data + j * eleSize, eleSize);
+        }
+    }
+
+    void launchTranspose(
+        KernelLaunchParameters const &params,
+        void const *data, DimStride const *strides, void *output,
+        unsigned int rank,
+        unsigned int eleSize) {
+        transposeKernel<<<
+            params.gridSize,
+            params.blockSize,
+            params.dynamicSharedBytes,
+            reinterpret_cast<cudaStream_t>(params.stream)>>>(
+            params.n,
+            reinterpret_cast<uint8_t const *>(data),
+            strides,
+            reinterpret_cast<uint8_t *>(output),
+            rank,
+            eleSize);
+    }
+
+}// namespace refactor::kernel::cuda
