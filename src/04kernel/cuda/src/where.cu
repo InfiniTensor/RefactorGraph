d05ee11a1686b8ef7f4d3dd5151a1508ba18b2b0
YdrMaster <ydrml@hotmail.com>
2023-11-10 10:47:14 +0800

    perf(kernel): 手写 where kernel，放弃 thrust
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/04kernel/cuda/src/where.cu
@@ -0,0 +1,60 @@
+﻿#include "kernel/cuda/where.cuh"
+#include <cstdint>
+
+namespace refactor::kernel::cuda {
+
+    __global__ static void whereKernel(
+        unsigned long long n,
+        unsigned int const *strides,
+        bool const *c,
+        uint8_t const *x,
+        uint8_t const *y,
+        uint8_t *output,
+        unsigned int rank,
+        unsigned int eleSize) {
+        for (auto tid = blockIdx.x * blockDim.x + threadIdx.x,
+                  step = blockDim.x * gridDim.x;
+             tid < n;
+             tid += step) {
+            auto ic = 0u, ix = 0u, iy = 0u, rem = tid;
+            for (auto j = 0u; j < rank; ++j) {
+                auto dim = strides + 4 * j;
+                auto quot = rem / dim[3];
+                rem %= dim[3];
+                ic += quot * dim[0];
+                ix += quot * dim[1];
+                iy += quot * dim[2];
+            }
+            memcpy(output + tid * eleSize,
+                   c[ic]
+                       ? x + ix * eleSize
+                       : y + iy * eleSize,
+                   eleSize);
+        }
+    }
+
+    void launchWhere(
+        KernelLaunchParameters const &params,
+        unsigned int const *strides,
+        void const *c,
+        void const *x,
+        void const *y,
+        void *output,
+        unsigned int rank,
+        unsigned int eleSize) {
+        whereKernel<<<
+            params.gridSize,
+            params.blockSize,
+            params.dynamicSharedBytes,
+            reinterpret_cast<cudaStream_t>(params.stream)>>>(
+            params.n,
+            strides,
+            reinterpret_cast<bool const *>(c),
+            reinterpret_cast<uint8_t const *>(x),
+            reinterpret_cast<uint8_t const *>(y),
+            reinterpret_cast<uint8_t *>(output),
+            rank,
+            eleSize);
+    }
+
+}// namespace refactor::kernel::cuda
--- src/04kernel/src/kernels/where/where_cuda.cu
+++ src/04kernel/src/kernels/where/where_cuda.cu
@@ -1,54 +1,23 @@
+#include "kernel/cuda/where.cuh"
 #include "where_cuda.hh"
 #include <thrust/device_vector.h>
-#include <thrust/execution_policy.h>
-#include <thrust/for_each.h>
-#include <thrust/host_vector.h>
 
 namespace refactor::kernel {
     using namespace runtime;
 
-    // NOTICE NVCC 编译的对象无论是否在相同编译单元，都不可以重名。
-    struct WhereKernelFunctor {
-        bool const *c;
-        uint8_t const *x;
-        uint8_t const *y;
-        uint8_t *output;
-        uint_lv2 const *strides;
-        long rank, eleSize;
-
-        __device__ void operator()(long i) const noexcept {
-            auto ic = 0l, ix = 0l, iy = 0l, rem = i;
-            for (auto j = 0l; j < rank; ++j) {
-                auto dim = strides + 4 * j;
-                auto quot = rem / dim[3];
-                rem %= dim[3];
-                ic += quot * dim[0];
-                ix += quot * dim[1];
-                iy += quot * dim[2];
-            }
-            memcpy(output + i * eleSize,
-                   c[ic]
-                       ? x + ix * eleSize
-                       : y + iy * eleSize,
-                   eleSize);
-        }
-    };
-
     Routine WhereCuda::lower(Resources &) const noexcept {
         return [strides = thrust::device_vector<uint_lv2>(broadcaster.strides.begin(), broadcaster.strides.end()),
-                n = static_cast<long>(broadcaster.outputsCount),
+                params = cuda::ThreadsDistributer()(broadcaster.outputsCount),
                 eleSize = static_cast<long>(dataType.size())](Resources &res, void const **inputs, void **outputs) {
-            thrust::for_each_n(
-                thrust::device, thrust::counting_iterator<long>(0), n,
-                WhereKernelFunctor{
-                    reinterpret_cast<bool const *>(inputs[0]),
-                    reinterpret_cast<uint8_t const *>(inputs[1]),
-                    reinterpret_cast<uint8_t const *>(inputs[2]),
-                    reinterpret_cast<uint8_t *>(outputs[0]),
-                    strides.data().get(),
-                    static_cast<long>(strides.size() / 4),
-                    eleSize,
-                });
+            cuda::launchWhere(
+                params,
+                strides.data().get(),
+                inputs[0],
+                inputs[1],
+                inputs[2],
+                outputs[0],
+                strides.size() / 4,
+                eleSize);
         };
     }
 
