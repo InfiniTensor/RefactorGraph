365f02a51781b3c5e184d5019ff7837939fb712e
YdrMaster <ydrml@hotmail.com>
2023-11-20 17:20:39 +0800

    fix(mem_manager): 为 0 大小的分配请求返回空指针，并且释放空指针总是安全的
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/02mem_manager/src/mem_pool.cc
+++ src/02mem_manager/src/mem_pool.cc
@@ -10,14 +10,16 @@ namespace refactor::mem_manager {
     MemPool::~MemPool() {
         _f->free(_ptr);
     }
-    void *MemPool::malloc(size_t bytes) noexcept {
+    void *MemPool::malloc(size_t const bytes) noexcept {
+        if (bytes == 0) { return nullptr; }
         auto offset = _calculator.alloc(bytes);
         ASSERT(_calculator.peak() < _memPoolSize, "out of memory");
         void *ans = static_cast<uint8_t *>(_ptr) + offset;
         _ptrToBlobsize.emplace(ans, bytes);
         return ans;
     }
-    void MemPool::free(void *ptr) {
+    void MemPool::free(void *const ptr) {
+        if (!ptr) { return; }
         auto it = _ptrToBlobsize.find(ptr);
         ASSERT(it != _ptrToBlobsize.end(), "invalid ptr");
         auto offset = static_cast<uint8_t *>(ptr) - static_cast<uint8_t *>(_ptr);
