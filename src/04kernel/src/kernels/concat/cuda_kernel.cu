7ff965d1f962e26f00a6765a83c808a066241f86
YdrMaster <ydrml@hotmail.com>
2023-11-22 09:11:54 +0800

    fix(kernel): 避免在 concat/split 启动时同步拷贝内存
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/04kernel/src/kernels/concat/cuda_kernel.cu
+++ src/04kernel/src/kernels/concat/cuda_kernel.cu
@@ -8,14 +8,26 @@ namespace refactor::kernel {
     using namespace runtime;
 
     auto ConcatCuda::lower(Resources &) const noexcept -> RoutineWorkspace {
-        auto sub = std::min(info.submultiple(), 16u);
         auto workspaceSize = info.segments.size() * sizeof(void *);
+        struct Workspace {
+            void *pageLocked;
+            size_t size;
+
+            Workspace(size_t size) : size(size) {
+                cudaMallocHost(&pageLocked, size);
+            }
+            ~Workspace() {
+                cudaFreeHost(pageLocked);
+            }
+        };
+        auto sub = std::min(info.submultiple(), 32u);
         auto routine = [params = cuda::ThreadsDistributer()(info.blockCount * info.sum / sub),
                         segments = thrust::device_vector<dim_t>(info.segments.begin(), info.segments.end()),
-                        workspaceSize,
+                        workspace_ = std::make_shared<Workspace>(workspaceSize),
                         sum = info.sum / sub,
                         sub](Resources &res, void *workspace, void const *const *inputs, void *const *outputs) {
-            cudaMemcpy(workspace, inputs, workspaceSize, cudaMemcpyHostToDevice);
+            std::memcpy(workspace_->pageLocked, inputs, workspace_->size);
+            cudaMemcpyAsync(workspace, workspace_->pageLocked, workspace_->size, cudaMemcpyHostToDevice);
             cuda::launchConcat(
                 params,
                 reinterpret_cast<void const **>(workspace),
