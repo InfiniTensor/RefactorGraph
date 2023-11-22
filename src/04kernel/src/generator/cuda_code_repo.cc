a5a5671109047de392c615915c592e73271b7160
YdrMaster <ydrml@hotmail.com>
2023-11-22 12:54:37 +0800

    feat(kernel): 添加 nvcc codegen 基础设施
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/04kernel/src/generator/cuda_code_repo.cc
@@ -0,0 +1,33 @@
+﻿#include "cuda_code_repo.hh"
+#include "common.h"
+#include <dlfcn.h>
+
+namespace refactor::kernel {
+
+    auto CudaCodeRepo::hardware() const noexcept -> std::string_view {
+        return "CUDA";
+    }
+    auto CudaCodeRepo::extension() const noexcept -> std::string_view {
+        return "cu";
+    }
+    void *CudaCodeRepo::_compile(std::filesystem::path const &src,
+                                 const char *symbol) {
+        auto out = src, so = src;
+        out.replace_extension("o");
+        so.replace_filename("libkernel.so");
+        {
+            std::string command;
+            command = fmt::format("nvcc -Xcompiler \"-fPIC\" {} -c -o {}", src.c_str(), out.c_str());
+            std::system(command.c_str());
+            command = fmt::format("nvcc -shared {} -o {}", out.c_str(), so.c_str());
+            std::system(command.c_str());
+        }
+
+        auto handle = dlopen(so.c_str(), RTLD_LAZY);
+        ASSERT(handle, "Failed to load kernel library: {}", dlerror());
+        auto function = dlsym(handle, symbol);
+        ASSERT(function, "Failed to load kernel function: {}", dlerror());
+        return function;
+    }
+
+}// namespace refactor::kernel
---
+++ src/04kernel/src/generator/cuda_code_repo.hh
@@ -0,0 +1,18 @@
+﻿#ifndef KRENEL_CUDA_CODE_REPO_HH
+#define KRENEL_CUDA_CODE_REPO_HH
+
+#include "kernel/generator/code_repo.h"
+
+namespace refactor::kernel {
+
+    class CudaCodeRepo final : public CodeRepo {
+    protected:
+        std::string_view hardware() const noexcept final;
+        std::string_view extension() const noexcept final;
+        void *_compile(std::filesystem::path const &src,
+                       const char *symbol) final;
+    };
+
+}// namespace refactor::kernel
+
+#endif// KRENEL_CUDA_CODE_REPO_HH
