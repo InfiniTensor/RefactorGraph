b6776229004b15a8a1e02b7f9acccd207ad0b94a
YdrMaster <ydrml@hotmail.com>
2023-12-05 14:54:40 +0800

    build(kernel): 移除一些没有必要的 cu 文件
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/04kernel/src/kernels/concat/cuda_kernel.cc
+++ src/04kernel/src/kernels/concat/cuda_kernel.cc
@@ -1,5 +1,13 @@
 ﻿#include "cuda_kernel.hh"
 
+#ifdef USE_CUDA
+#include "../../generator/cuda_code_repo.hh"
+#include "cuda_kernel.hh"
+#include "kernel/cuda/threads_distributer.cuh"
+#include <cuda_runtime.h>
+#include <sstream>
+#endif
+
 namespace refactor::kernel {
     using K = ConcatCuda;
 
@@ -23,5 +31,98 @@ namespace refactor::kernel {
     auto K::description() const noexcept -> std::string_view {
         return "Performing split operation using CUDA";
     }
+
+#ifdef USE_CUDA
+
+    constexpr static const char *TEMPLATE = R"~(
+struct Inputs {{
+    char const *const addr[{0:}];
+}};
+
+__global__ static void splitKernel(void *output, Inputs inputs) {{
+    using T = {1:};
+
+    constexpr static unsigned int
+        sum = {2:},
+        segments[]{{{3:}}};
+    auto dst = reinterpret_cast<T *>(output);
+
+    for (auto tid = blockIdx.x * blockDim.x + threadIdx.x,
+              step = blockDim.x * gridDim.x;
+         tid < {4:};
+         tid += step) {{
+        auto i = tid % sum * static_cast<unsigned int>(sizeof(T)), j = 0u;
+        while (i >= segments[j]) i -= segments[j++];
+        dst[tid] = *reinterpret_cast<T const *>(inputs.addr[j] + (tid / sum) * segments[j] + i);
+    }}
+}}
+
+extern "C" {{
+
+void launchKernel(void const *const *inputs, void *output) {{
+    splitKernel<<<{5:}, {6:}>>>(
+        output,
+        {{{7:}
+        }});
+}}
+
+}}
+)~";
+
+    auto K::lower(Resources &) const noexcept -> RoutineWorkspace {
+        using namespace runtime;
+        if (info.blockCount == 1) {
+            return [info = this->info](Resources &, void *, void const *const *inputs, void *const *outputs) {
+                auto dst = reinterpret_cast<uint8_t *>(outputs[0]);
+                for (auto i : range0_(info.segments.size())) {
+                    auto size = info.segments[i];
+                    cudaMemcpyAsync(dst, inputs[i], size, cudaMemcpyDeviceToDevice);
+                    dst += size;
+                }
+            };
+        }
+
+        auto unit = info.unit(16);
+        auto params = cuda::ThreadsDistributer()(info.blockCount * info.sum / unit);
+        auto inputCount = info.segments.size();
+
+        std::stringstream ss;
+        for (auto seg : info.segments) {
+            ss << seg << ", ";
+        }
+        auto segments = ss.str();
+
+        ss.str("");
+        for (auto i : range0_(inputCount)) {
+            ss << std::endl
+               << "            reinterpret_cast<char const *>(inputs[" << i << "]), ";
+        }
+        auto castInputs = ss.str();
+
+        ss.str("");
+        ss << "Concat_" << info.blockCount << ',' << unit;
+        for (auto seg : info.segments) {
+            ss << ',' << seg;
+        }
+        auto name = ss.str();
+        auto code = fmt::format(
+            TEMPLATE,
+            inputCount,                     // 0
+            CudaCodeRepo::memCopyType(unit),// 1
+            info.sum / unit,                // 2
+            segments,                       // 3
+            params.n,                       // 4
+            params.gridSize,                // 5
+            params.blockSize,               // 6
+            castInputs                      // 7
+        );
+
+        using Fn = void (*)(void const *const *, void *);
+        auto function = reinterpret_cast<Fn>(CudaCodeRepo::compile_(name.c_str(), code.c_str(), "launchKernel"));
+        return [function](Resources &, void *, void const *const *inputs, void *const *outputs) {
+            function(inputs, outputs[0]);
+        };
+    }
+#endif
 
 }// namespace refactor::kernel
--- src/04kernel/src/kernels/reduce/cudnn_kernel.cc
+++ src/04kernel/src/kernels/reduce/cudnn_kernel.cc
@@ -1,5 +1,10 @@
 #include "cudnn_kernel.hh"
-#include "common.h"
+
+#ifdef USE_CUDA
+#include "../../utilities/cuda/cudnn_context.hh"
+#include "../../utilities/cuda/cudnn_functions.h"
+#include "hardware/functions.h"
+#endif
 
 namespace refactor::kernel {
     using K = ReduceCudnn;
@@ -35,5 +40,85 @@ namespace refactor::kernel {
     auto K::description() const noexcept -> std::string_view {
         return "Performing reduce operation using CUDNN";
     }
+
+#ifdef USE_CUDA
+
+    auto ReduceCudnn::lower(Resources &res) const -> RoutineWorkspace {
+        using namespace cudnn;
+        using namespace runtime;
+
+        // RAII for closure
+        struct Descriptors {
+            cudnnTensorDescriptor_t x;
+            cudnnTensorDescriptor_t y;
+            cudnnReduceTensorDescriptor_t reduce;
+
+            Descriptors() {
+                CUDNN_ASSERT(cudnnCreateTensorDescriptor(&x));
+                CUDNN_ASSERT(cudnnCreateTensorDescriptor(&y));
+                CUDNN_ASSERT(cudnnCreateReduceTensorDescriptor(&reduce));
+            }
+            ~Descriptors() noexcept(false) {
+                // Destories in CUDA does not require sync.
+                // But cuDNN does not state whether sync is required before destories.
+                CUDNN_ASSERT(cudnnDestroyTensorDescriptor(x));
+                CUDNN_ASSERT(cudnnDestroyTensorDescriptor(y));
+                CUDNN_ASSERT(cudnnDestroyReduceTensorDescriptor(reduce));
+            }
+
+            Descriptors(const Descriptors &) = delete;
+            Descriptors(Descriptors &&) = delete;
+        };
+        auto d = std::make_shared<Descriptors>();
+
+        std::vector<int>
+            dimsI(shape.begin(), shape.end()),
+            dimsO(shape.begin(), shape.end());
+        for (auto axis : axes) {
+            dimsO[axis] = 1;
+        }
+        setCudnnTensor(d->x, dataType, slice(dimsI.data(), dimsI.size()));
+        setCudnnTensor(d->y, dataType, slice(dimsO.data(), dimsO.size()));
+
+        // clang-format off
+        auto reduceOp = reduceType == ReduceType::Mean ? CUDNN_REDUCE_TENSOR_AVG
+                      : reduceType == ReduceType::Sum  ? CUDNN_REDUCE_TENSOR_ADD
+                      : reduceType == ReduceType::Min  ? CUDNN_REDUCE_TENSOR_MIN
+                      : reduceType == ReduceType::Max  ? CUDNN_REDUCE_TENSOR_MAX
+                      : reduceType == ReduceType::L1   ? CUDNN_REDUCE_TENSOR_NORM1
+                      : reduceType == ReduceType::L2   ? CUDNN_REDUCE_TENSOR_NORM2
+                      : reduceType == ReduceType::Prod ? CUDNN_REDUCE_TENSOR_MUL
+                      : UNREACHABLEX(cudnnReduceTensorOp_t, "");
+        // clang-format on
+        CUDNN_ASSERT(cudnnSetReduceTensorDescriptor(
+            d->reduce, reduceOp, cudnnDataTypeConvert(dataType),
+            CUDNN_NOT_PROPAGATE_NAN, CUDNN_REDUCE_TENSOR_NO_INDICES, CUDNN_32BIT_INDICES));
+
+        auto handler = res.fetchOrStore<CudnnContext>()->handle;
+        size_t idxWorkspaceSize, workspaceSize;
+        // get workspace
+        CUDNN_ASSERT(cudnnGetReductionIndicesSize(handler, d->reduce, d->x, d->y, &idxWorkspaceSize));
+        CUDNN_ASSERT(cudnnGetReductionWorkspaceSize(handler, d->reduce, d->x, d->y, &workspaceSize));
+        idxWorkspaceSize = hardware::alignBytes(idxWorkspaceSize, 256);
+
+        // nvcc at c++11 doesn't support real move capture
+        auto routine = [d = std::move(d),
+                        idxWorkspaceSize,
+                        workspaceSize](Resources &res, void *workspace, void const *const *inputs, void *const *outputs) {
+            void *idxWorkspace = workspace,
+                 *dataWorkspace = reinterpret_cast<uint8_t *>(workspace) + idxWorkspaceSize;
+            float alpha = 1, beta = 0;
+            CUDNN_ASSERT(cudnnReduceTensor(
+                res.fetchOrStore<CudnnContext>()->handle,
+                d->reduce,
+                idxWorkspace, idxWorkspaceSize,
+                dataWorkspace, workspaceSize,
+                &alpha, d->x, inputs[0],
+                &beta, d->y, outputs[0]));
+        };
+        return RoutineWorkspace(std::move(routine), idxWorkspaceSize + workspaceSize);
+    }
+
+#endif
 
 }// namespace refactor::kernel
