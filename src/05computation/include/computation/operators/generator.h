38148b98d53438d2a76fd533fb9a8b5f8c84e575
YdrMaster <ydrml@hotmail.com>
2023-12-28 16:28:10 +0800

    feat(computation): 添加 generator operator
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/05computation/include/computation/operators/generator.h
@@ -0,0 +1,23 @@
+﻿#ifndef COMPUTATION_GENERATOR_H
+#define COMPUTATION_GENERATOR_H
+
+#include "../graph.h"
+#include "../operator.h"
+
+namespace refactor::computation {
+
+    struct Generator final : public Operator {
+
+        Graph subgraph;
+
+        explicit Generator(decltype(subgraph));
+
+        static size_t typeId() noexcept;
+        size_t opTypeId() const noexcept final;
+        std::string_view name() const noexcept final;
+        kernel::CollectorBox candidateKernels(Target) const noexcept final;
+    };
+
+}// namespace refactor::computation
+
+#endif// COMPUTATION_GENERATOR_H
