88e0712aee470e4a4d7d18396bc5b0c628e0557a
YdrMaster <ydrml@hotmail.com>
2024-01-23 17:34:06 +0800

    build: 添加 llm 子项目用于前端大模型自定义算子
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

---
+++ src/08-01llm/src/operators.cpp
@@ -0,0 +1,14 @@
+﻿#include "llm/operators.h"
+#include "operators/rms_normalization.hh"
+
+namespace refactor::llm {
+    using namespace frontend;
+
+    void register_() {
+        // clang-format off
+        #define REGISTER(NAME, CLASS) Operator::register_<CLASS>("llm::" #NAME)
+        #undef REGISTER
+        // clang-format on
+    }
+
+}// namespace refactor::llm
---
+++ src/08-01llm/src/operators/rms_normalization.cc
@@ -0,0 +1,24 @@
+﻿#include "rms_normalization.hh"
+#include "common.h"
+
+namespace refactor::llm {
+    using Op = RmsNormalization;
+
+    auto Op::build(ModelContext const &, std::string_view, Attributes) -> OpBox {
+        return OpBox(std::make_unique<Op>());
+    }
+    auto Op::typeId() -> size_t {
+        static uint8_t ID = 1;
+        return reinterpret_cast<size_t>(&ID);
+    }
+
+    auto Op::opTypeId() const -> size_t { return typeId(); }
+    auto Op::opTypeName() const -> std::string_view { return "llm::RmsNormalization"; }
+
+    auto Op::infer(TensorRefs inputs, InferOptions const &) const -> InferResult {
+        EXPECT_SIZE(2)
+
+        TODO("");
+    }
+
+}// namespace refactor::llm
