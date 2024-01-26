c462d7ddcfa35b110f73444013756dabe74f13fb
YdrMaster <ydrml@hotmail.com>
2024-01-26 14:04:37 +0800

    fix(kernel): 引入最新版本的 cccl 以支持 nvrtc 与 cub 配合使用
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/04kernel/src/kernels/rms_normalization/cuda_kernel.cc
+++ src/04kernel/src/kernels/rms_normalization/cuda_kernel.cc
@@ -48,31 +48,27 @@ namespace refactor::kernel {
 
     // 0: data type
     // 1: block size
-    // 2: epsilon cast
+    // 2: T -> float
+    // 3: T <- float
     constexpr static const char *TEMPLATE = R"~(
-#include <cub/cub.cuh>
-
-static __device__ __forceinline__ {0:} squareSum({0:} a, {0:} b) {{
-    return a * a + b * b;
-}}
+#include <cub/block/block_reduce.cuh>
 
 extern "C" __global__ void kernel(
-    {0:} *__restrict__ const y,
-    {0:} const *__restrict__ const x,
-    {0:} const *__restrict__ const w,
-    float epsilon_) {{
+    {0:} *__restrict__ y,
+    {0:} const *__restrict__ x,
+    {0:} const *__restrict__ w,
+    float epsilon) {{
 
-    auto epsilon = {2:}(epsilon_);
     x += blockIdx.x * blockDim.x + threadIdx.x;
-    y += blockIdx.x * blockDim.x + threadIdx.x;;
+    y += blockIdx.x * blockDim.x + threadIdx.x;
     w += threadIdx.x;
 
     using BlockReduce = cub::BlockReduce<{0:}, {1:}>;
     __shared__ typename BlockReduce::TempStorage tempStorage;
     __shared__ {0:} rms;
-    auto acc = BlockReduce(tempStorage).Reduce(*x, squareSum);
+    auto acc = BlockReduce(tempStorage).Reduce(*x * *x, cub::Sum());
     if (threadIdx.x == 0) {{
-        rms = rsqrt(acc / blockDim.x + epsilon);
+        rms = {3:}(rsqrt({2:}(acc) / blockDim.x + epsilon));
     }}
     __syncthreads();
 
@@ -96,6 +92,11 @@ extern "C" __global__ void kernel(
           : dataType == DataType::F64  ? "static_cast<float>"
           : dataType == DataType::FP16 ? "__half2float"
           : dataType == DataType::BF16 ? "__bfloat162float"
+          : UNREACHABLEX(const char*, "unreachable"),
+            dataType == DataType::F32  ? ""
+          : dataType == DataType::F64  ? ""
+          : dataType == DataType::FP16 ? "__float2half"
+          : dataType == DataType::BF16 ? "__float2bfloat16"
           : UNREACHABLEX(const char*, "unreachable")
             // clang-format on
         );
