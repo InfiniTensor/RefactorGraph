e54c7db38cecea327bb75d249ed223f2b332279c
YdrMaster <ydrml@hotmail.com>
2024-02-02 12:53:06 +0800

    refactor(kernel): 实现一种不依赖模板参数的 BlockReduce 并用于 softmax
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/04kernel/cuda/include/kernel/cuda/reduce.cuh
+++ src/04kernel/cuda/include/kernel/cuda/reduce.cuh
@@ -4,6 +4,28 @@
 #include <cub/warp/warp_reduce.cuh>
 
 namespace refactor::kernel::cuda {
-}
+
+    template<class T, class ReductionOp>
+    __inline__ __device__ T blockReduce(T x, T init, ReductionOp op) {
+        using WarpReduce = cub::WarpReduce<T>;
+        __shared__ typename WarpReduce::TempStorage tempStorage;
+        __shared__ T shared[32], ans;
+
+        auto reduce = WarpReduce(tempStorage);
+        int lane = threadIdx.x % 32;
+        int wid = threadIdx.x / 32;
+        x = reduce.Reduce(x, op);
+        if (lane == 0) { shared[wid] = x; }
+        __syncthreads();
+        if (wid == 0) {
+            x = (threadIdx.x < blockDim.x / 32) ? shared[lane] : init;
+            shared[lane] = reduce.Reduce(x, op);
+            if (lane == 0) { ans = shared[0]; }
+        }
+        __syncthreads();
+        return ans;// avoid RAW hazard
+    }
+
+}// namespace refactor::kernel::cuda
 
 #endif// KERNEL_CUDA_REDUCE_CUH
--- src/04kernel/test/kernels/softmax/test_cuda.cpp
+++ src/04kernel/test/kernels/softmax/test_cuda.cpp
@@ -4,18 +4,19 @@
 #include "../../../src/kernels/softmax/cuda_kernel.hh"
 #include "hardware/device_manager.h"
 #include <gtest/gtest.h>
+#include <numeric>
 
 using namespace refactor;
 using namespace kernel;
 using namespace hardware;
 
-TEST(kernel, SoftmaxCuda) {
+static void test(Shape shape, int axis) {
     // build routine
-    auto xTensor = Tensor::share(DataType::F32, Shape{2, 3, 2, 5, 4});
-    auto outTensor = Tensor::share(DataType::F32, Shape{2, 3, 2, 5, 4});
-    dim_t axis = 1;
-    auto kCpu = SoftmaxCpu::build(SoftmaxInfo(*xTensor, axis));
-    auto kCuda = SoftmaxCuda::build(SoftmaxInfo(*xTensor, axis));
+    auto xTensor = Tensor::share(DataType::F32, shape);
+    auto outTensor = Tensor::share(DataType::F32, shape);
+    SoftmaxInfo info(*xTensor, axis);
+    auto kCpu = SoftmaxCpu::build(info);
+    auto kCuda = SoftmaxCuda::build(info);
     ASSERT_TRUE(kCpu && kCuda);
     auto res = runtime::Resources();
     auto rCpu = kCpu->lower(res).routine;
@@ -28,6 +29,7 @@ TEST(kernel, SoftmaxCuda) {
     std::vector<float>
         data(xTensor->elementsSize(), 0),
         cpuOut(outTensor->elementsSize());
+    std::iota(data.begin(), data.end(), 0);
     gpuIn->copyFromHost(data.data(), xTensor->bytesSize());
     // inference
     {
@@ -47,6 +49,11 @@ TEST(kernel, SoftmaxCuda) {
     for (auto i : range0_(result.size())) {
         EXPECT_FLOAT_EQ(cpuOut[i], result[i]);
     }
+}
+
+TEST(kernel, SoftmaxCuda) {
+    test({2, 3, 2, 5, 4}, 1);
+    test({2, 2048, 2, 5, 4}, 1);
 }
 
 #endif
