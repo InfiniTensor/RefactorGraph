e838327ae7450b7b6dc49c0b1f43bf57288d53db
xgqdut2016 <kenan_gewei@163.com>
2024-03-01 07:09:31 +0000

    copy cnnlSoftmax

---
+++ src/04kernel/src/utilities/bang/cnrt_functions.cc
@@ -0,0 +1,28 @@
+#ifdef USE_BANG
+#include "cnrt_functions.h"
+#include "cnnl_functions.h"
+#include <cnrt.h>
+#include <cstdio>
+
+namespace refactor::kernel::bang {
+
+    int currentDevice() {
+        int device;
+        BANG_ASSERT(cnrtGetDevice(&device));
+        return device;
+    }
+
+    void sync() {
+        BANG_ASSERT(cnrtSyncDevice());
+    }
+
+    void copyOut(void *dst, const void *src, size_t size) {
+        sync();
+        BANG_ASSERT(cnrtMemcpy(dst, const_cast<void *>(src), size,
+                               CNRT_MEM_TRANS_DIR_DEV2HOST));
+    }
+
+}// namespace refactor::kernel::bang
+
+#endif
+
